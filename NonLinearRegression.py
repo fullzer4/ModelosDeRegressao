dde9cd44655dad277cdb93c5dd4c6135ad2e9cd4
Gabriel Pelizzaro Pereira <53914594+fullzer4@users.noreply.github.com>
2022-10-04 21:43:57 -0300

    att

--- NonLinearRegression.py
+++ NonLinearRegression.py
@@ -1,7 +1,11 @@
+# Imports
+
 import numpy as np
 import pandas as pd
 from scipy.optimize import curve_fit
 from sklearn.metrics import r2_score
+
+# Preparando dados
 
 df = pd.read_csv("./china_gdp.csv")
 
@@ -18,7 +22,7 @@ popt, pcov = curve_fit(sigmoid, xdata, ydata)
 
 print(" beta_1 = %f, beta_2 = %f" % (popt[0], popt[1])) # mostrando parametros finais
 
-# train test
+# Definindo treino e teste
 
 msk = np.random.rand(len(df)) < 0.8
 train_x = xdata[msk]
@@ -26,13 +30,13 @@ test_x = xdata[~msk]
 train_y = ydata[msk]
 test_y = ydata[~msk]
 
-# build the model using train set
+# Construindo modelo usando train set
 popt, pcov = curve_fit(sigmoid, train_x, train_y)
 
-# predict using test set
+# Prevendo usando test set
 y_hat = sigmoid(test_x, *popt)
 
-# avaliando
+# Avaliando o nosso modelo
 print("Mean absolute error: %.2f" % np.mean(np.absolute(y_hat - test_y)))
 print("Residual sum of squares (MSE): %.2f" % np.mean((y_hat - test_y) ** 2))
 print("R2-score: %.2f" % r2_score(test_y,y_hat) )
